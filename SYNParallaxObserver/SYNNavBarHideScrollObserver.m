f077328418b9b879bf765bfc1a8e9957483d480a
Sam Corder <samus@codeargyle.com>
2014-03-15 12:01:29 -0400

    Fix for unhiding on bottom vertical bounce.

--- SYNParallaxObserver/SYNNavBarHideScrollObserver.m
+++ SYNParallaxObserver/SYNNavBarHideScrollObserver.m
@@ -23,80 +23,88 @@
 - (instancetype)initWithObservedScrollView:(UIScrollView *)observedScrollView navigationController:(UINavigationController *)navigationController
 {
     self = [super initWithObservedScrollView:observedScrollView];
+
     if (self) {
         self.navController = navigationController;
         self.navBarOrigin = navigationController.navigationBar.frame.origin;
         self.lastOffsetPoint = CGPointZero;
-        
+
         minY = -24;
         maxY = self.navBarOrigin.y;
     }
+
     return self;
 }
 
-- (void)observedContentOffsetChanged:(CGPoint) point
+- (void)observedContentOffsetChanged:(CGPoint)point
 {
-    if ([self isStretchingAtPoint:point]) {
+    if ([self isBouncingAtPoint:point]) {
         return;
     }
-    
+
     float distance = [self distanceScrolled:point];
     float alpha = [self shiftNavBar:self.navController.navigationBar verticalPoints:distance];
     [self setNavBarSubViewsAlpha:alpha];
-    
+
     self.lastOffsetPoint = point;
 }
 
-- (float)distanceScrolled:(CGPoint) point
+- (float)distanceScrolled:(CGPoint)point
 {
     return self.lastOffsetPoint.y - point.y;
 }
 
-- (BOOL)isStretchingAtPoint:(CGPoint) point
+- (BOOL)isBouncingAtPoint:(CGPoint)point
 {
-    float y = point.y * -1;
-    return y >= self.observedScrollView.contentInset.top;
+    BOOL bouncingTop = point.y * -1 >= self.observedScrollView.contentInset.top;
+    BOOL bouncingBottom = (point.y >= (self.observedScrollView.contentSize.height - self.observedScrollView.bounds.size.height));
+
+    return bouncingTop || bouncingBottom;
 }
 
-- (float)shiftNavBar:(UINavigationBar *)navBar verticalPoints:(float) vpoints
+- (float)shiftNavBar:(UINavigationBar *)navBar verticalPoints:(float)vpoints
 {
-
     CGRect barRect = self.navController.navigationBar.frame;
+
     CGPoint barPt = barRect.origin;
+
     barPt.y += vpoints;
+
     if (barPt.y <= minY) {
         barPt.y = minY;
-    }else if (barPt.y >= maxY) {
+    } else if (barPt.y >= maxY) {
         barPt.y = maxY;
     }
-    
+
     barRect.origin = barPt;
     navBar.frame = barRect;
-    
+
     return [self percentOfMaxAtPoint:barPt];
 }
 
-- (float)percentOfMaxAtPoint:(CGPoint) point
+- (float)percentOfMaxAtPoint:(CGPoint)point
 {
     float min = 0;
     float max = maxY - min;
     float y = point.y - min;
-    float percentage = y/max;
+    float percentage = y / max;
+
     percentage = percentage > 1.0 ? 1.0 : percentage; //Limit to 1.0
-    
+
     return percentage;
 }
 
-- (void)setNavBarSubViewsAlpha:(float) alpha
+- (void)setNavBarSubViewsAlpha:(float)alpha
 {
     for (UIView *sub in [self.navController.navigationBar subviews]) {
         NSString *name = NSStringFromClass([sub class]);
-        if ([name isEqualToString:@"_UINavigationBarBackground"] || [name isEqualToString:@"_UINavigationBarBackIndicatorView"]){
+
+        if ([name isEqualToString:@"_UINavigationBarBackground"] || [name isEqualToString:@"_UINavigationBarBackIndicatorView"]) {
             continue;
         }
+
         sub.alpha = alpha;
     }
-
 }
 
 @end
